ba29e4276906629fbdfbfd437cbdb0b90772bd4a
daiya0324 <daiya19980324@gmail.com>
2022-03-21 15:26:23 +0900

    施術データ保存

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :neme, :day, :manager, :cut_id, :color_id, :parm_id, :treatment_id, :description, presence: true
+  validates :name, :day, :manager, :cut_id, :color_id, :parm_id, :treatment_id, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :cut
